6c376ee5b9209a181ed32e7d1f01be9b7e92df39
Ajithdev444 <Ajithdev444@gamil.com>
2022-11-23 10:37:31 +0530

    navbar heading subheading

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { MdOutlineRestaurantMenu } from 'react-icons/md'
 import images from '../../constants/images';
 
 import './Navbar.css';
 
-const Navbar = () => (
+const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false)
+  return (
   <div className='app__navbar'>
     <div className='app__navbar-logo'>
       <img src={images.gericht} alt='app logo'/>
@@ -17,6 +20,29 @@ const Navbar = () => (
       <li className='p__opensans'><a href='#awards'>Awards</a></li>
       <li className='p__opensans'><a href='#contact'>Contact</a></li>
     </ul>
+    <div className='app__navbar-login'>
+      <a href='#login' className='p__opensans'>Login / Register</a>
+      <div/>
+      <a href='/' className='p__opensans'>Book Table</a>
+    </div>
+
+    <div className='app__navbar-smallscreen'>
+      <GiHamburgerMenu className='overlay__open' color='#fff' fontSize={27} onClick={()=>setToggleMenu(true)}/>
+      { toggleMenu && (
+      <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+        <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={()=>setToggleMenu(false)}/>
+        <ul className='app__navbar-smallscreen_links'>
+          <li className='p__opensans'><a href='#home'>Home</a></li>
+          <li className='p__opensans'><a href='#about'>About</a></li>
+          <li className='p__opensans'><a href='#menu'>Menu</a></li>
+          <li className='p__opensans'><a href='#awards'>Awards</a></li>
+          <li className='p__opensans'><a href='#contact'>Contact</a></li>
+         </ul>
+      </div>
+      )}
+
+    </div>
   </div>
 )
+}
 export default Navbar;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { images } from '../../constants';
+import { SubHeading } from '../../components' 
 
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding ' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Chase the new flavour' />
+      <h1 className='app__header-h1'>The Key to Fine Dinning</h1>
+     <p className='p__opensans' style={{ margin: '2rem 0'}}>
+      one of the best restaurent in Fort Cochin . You can 
+      taste italian, french and portuguese dishes here 
+      with an beautiful view of sea...Enjoy your meals with your
+      love one's...
+     </p>
+     <button type='button' className='custom__button'> Explore Menu</button>
+     </div>
+
+     <div className='app__wrapper_img'>
+      <img src={images.welcome} alt='header img'/>
+     </div>
   </div>
 );
 
