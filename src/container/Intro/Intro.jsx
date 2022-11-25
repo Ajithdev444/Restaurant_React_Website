7ce2edeb074852b3bd18f4ddeb734bd7d4758c60
Ajithdev444 <Ajithdev444@gamil.com>
2022-11-25 13:58:04 +0530

    intro

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Intro.css';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs'
+import { meal } from '../../constants'
 
-import { images } from '../../constants'
+const Intro = () =>{
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = React.useRef();
 
-const Intro = () => (
-  <div>
-    Intro
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo)
+
+    if(playVideo) {
+      vidRef.current.pause();
+    }else {
+      vidRef.current.play();
+    }
+  }
+
+
+  return (
+  <div className='app__video'>
+    <video 
+      src={meal}
+      ref={vidRef}
+      type='video/mp4'
+      loop
+      controls={false}
+      muted
+    />
+
+    <div className='app__video-overlay flex__center'>
+      <div className='app__video-overlay_circle flex__center'
+      onClick={handleVideo}>
+        {playVideo ? 
+          <BsPauseFill color='#fff' fontSize={30}/> : 
+          <BsFillPlayFill color='#fff' fontSize={30}/>
+        }
+      </div>
+    </div>
+    
   </div>
 );
+}
 
 export default Intro;
